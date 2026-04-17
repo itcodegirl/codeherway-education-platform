309164cbf62acfabdc30193bd5af86c6ea63c2a3
Jenna Zawaski <88835851+itcodegirl@users.noreply.github.com>
2026-04-17 10:32:06 -0500

    Layout update for app and main paths

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router';
+import { router } from './src/routes/router';
 
 export default function App() {
   return <RouterProvider router={router} />;
